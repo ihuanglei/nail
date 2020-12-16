4f3580fe85b4d483fa724f0f45a4b6592b2b2bce
huanglei <code.huanglei@gmail.com>
2020-12-16 11:15:02 +0800

    添加readme

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# Nail 通用http请求
+# Nail
 
 
-使用 okhttp3 作为底层
+基于OKHttp3 封装的工具类。支持配置Header、配置SSL、配置proxy、Cookie等
 
 
+```java
 
+NailRequest request = NailRequest.create()
+        .setHost("www.baidu.com")
+        .setPath("/s")
+        .addQuery("wd", "nail");
+NailResponse response = Nail.request(request);
+
+```
 
 
 
--- src/test/java/org/huanglei/nail/NailTest.java
+++ src/test/java/org/huanglei/nail/NailTest.java
@@ -3,9 +3,6 @@ package org.huanglei.nail;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class NailTest {
 
@@ -15,17 +12,14 @@ public class NailTest {
                 .setHost("www.baidu.com")
                 .setPath("/s")
                 .addQuery("wd", "nail");
-
-        Map<String, Object> options = new HashMap<>();
-        NailResponse response = Nail.request(request, options);
+        NailResponse response = Nail.request(request);
         Assert.assertEquals(200, response.getStatusCode());
     }
 
     @Test
     public void testPost() throws Exception {
         NailRequest request = NailRequest.create();
-        Map<String, Object> options = new HashMap<>();
-        NailResponse response = Nail.request(request, options);
+        NailResponse response = Nail.request(request);
         Assert.assertEquals(200, response.getStatusCode());
     }
 }
