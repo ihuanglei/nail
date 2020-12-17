517e7d7f7268d9397907c292d2d35809b926d284
huanglei <code.huanglei@gmail.com>
2020-12-17 18:40:09 +0800

    fix

---
+++ src/test/java/org/huanglei/nail/NailRequestTest.java
@@ -0,0 +1,12 @@
+package org.huanglei.nail;
+
+import org.junit.Test;
+
+public class NailRequestTest {
+
+    @Test
+    public void testRequest() {
+        NailRequest request = NailRequest.create().setProtocol(NailRequest.Protocol.HTTPS).setHost("www.baidu.com").setPath("/s").putQuery("wd", "123");
+        System.out.println(request);
+    }
+}
